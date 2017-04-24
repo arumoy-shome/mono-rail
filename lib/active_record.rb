349c8f33b23d737485a6ccabf302cfed086959fb
Arumoy Shome <arumoy.shome@gmail.com>
2017-04-24 19:33:03 -0400

    Introduce ActiveRecord::Base.find
    This commit introduces the find method to our `ActiveRecord::Base`
    class. The method expects an open connection to the db and simply relies
    on the `SqliteAdapter.execute` method to run the SQL query. The first
    result containing the attributes is selected and a new Post object is
    returned using the attributes above.

--- lib/active_record.rb
+++ lib/active_record.rb
@@ -1,19 +1,37 @@
+require 'active_record/connection_adapter'
+
 module ActiveRecord
   class Base
-    def self.abstract_class=(params)
+    def self.abstract_class=(flag)
       #Not implemented
     end
 
-    def initialize(params = {})
-      @params = params
+    def self.find(id)
+      # TODO: once validations are implemented
+      # remove the to_i conversion
+      attributes = connection.execute("SELECT * FROM posts WHERE id = #{id.to_i}").first
+
+      new(attributes)
+    end
+
+    def self.establish_connection(options)
+      @@connection = ConnectionAdapter::SqliteAdapter.new(options[:database])
+    end
+
+    def self.connection
+      @@connection
+    end
+
+    def initialize(attributes = {})
+      @attributes = attributes
     end
 
     def id
-      @params[:id]
+      @attributes[:id]
     end
 
-    def text
-      @params[:text]
+    def title
+      @attributes[:title]
     end
   end
 end
--- test/active_record_test.rb
+++ test/active_record_test.rb
@@ -5,17 +5,33 @@ require 'blog/app/models/application_record'
 require 'blog/app/models/post'
 
 class ActiveRecordTest < Minitest::Test
-  def test_#initialize_with_valid_params
+  def test_find_with_id_returns_post_with_matching_id
+    Post.establish_connection(database: "#{ __dir__ }/blog/db/development.sqlite3")
+
+    post = Post.find(1)
+    assert_kind_of Post, post
+    assert_equal 1, post.id
+    assert_equal "Blueberry Muffins", post.title
+  end
+
+  def test_establish_connection_creates_a_db_connection_and_returns_it
+    connection = Post.establish_connection(database: "#{ __dir__ }/blog/db/development.sqlite3")
+
+    assert connection
+    assert_kind_of ActiveRecord::ConnectionAdapter::SqliteAdapter, connection
+  end
+
+  def test_#initialize_with_valid_params_creates_object_with_params
     post = Post.new(id: 1, title: "My first post")
 
     assert_equal 1, post.id
-    assert_equal "My first post", post.text
+    assert_equal "My first post", post.title
   end
 
-  def test_#initialize_with_nil_params
+  def test_#initialize_with_nil_params_creates_object_with_nil_params
     post = Post.new(id: 2)
 
     assert_equal 2, post.id
-    assert_equal nil, post.text
+    assert_nil post.title
   end
 end
